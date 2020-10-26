3369b71bca7e57b3dfed51a0fc5b35c61c77b98d
Tobias730 <tobbes91@hotmail.com>
2020-10-26 12:27:20 +0100

    Initial commit

---
+++ src/main/java/se/lexicon/tobias/App.java
@@ -0,0 +1,12 @@
+package se.lexicon.tobias;
+
+/**
+ * Hello World!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ) { System.out.println( "Hello! My Name Is Tobias" );
+
+    }
+}
